49f96da56a8885fcc82942782a4437404ccbe3c1
Mathias Schaefer (molily) <molily@mailbox.org>
2018-07-09 17:37:39 +0200

    Update mock store helper.

--- src/app/spec-helpers/make-mock-store.spec-helper.ts
+++ src/app/spec-helpers/make-mock-store.spec-helper.ts
@@ -1,5 +1,4 @@
-import { Action } from '@ngrx/store';
-import { ReplaySubject } from 'rxjs';
+import { Observable, ReplaySubject } from 'rxjs';
 import { distinctUntilChanged, map, pluck } from 'rxjs/operators';
 
 export class MockStore<T> extends ReplaySubject<T> {
@@ -7,30 +6,30 @@ export class MockStore<T> extends ReplaySubject<T> {
   // https://github.com/ngrx/platform/blob/master/modules/store/src/store.ts
   // MIT-licensed like ngrx:
   // https://github.com/ngrx/platform/blob/master/LICENSE
-  select<K>(mapFn: (state: T) => K): MockStore<K>;
-  select<a extends keyof T>(key: a): MockStore<T[a]>;
+  select<K>(mapFn: (state: T) => K): Observable<K>;
+  select<a extends keyof T>(key: a): Observable<T[a]>;
   select<a extends keyof T, b extends keyof T[a]>(
     key1: a,
     key2: b
-  ): MockStore<T[a][b]>;
+  ): Observable<T[a][b]>;
   select<a extends keyof T, b extends keyof T[a], c extends keyof T[a][b]>(
     key1: a,
     key2: b,
     key3: c
-  ): MockStore<T[a][b][c]>;
+  ): Observable<T[a][b][c]>;
   select<
     a extends keyof T,
     b extends keyof T[a],
     c extends keyof T[a][b],
     d extends keyof T[a][b][c]
-  >(key1: a, key2: b, key3: c, key4: d): MockStore<T[a][b][c][d]>;
+  >(key1: a, key2: b, key3: c, key4: d): Observable<T[a][b][c][d]>;
   select<
     a extends keyof T,
     b extends keyof T[a],
     c extends keyof T[a][b],
     d extends keyof T[a][b][c],
     e extends keyof T[a][b][c][d]
-  >(key1: a, key2: b, key3: c, key4: d, key5: e): MockStore<T[a][b][c][d][e]>;
+  >(key1: a, key2: b, key3: c, key4: d, key5: e): Observable<T[a][b][c][d][e]>;
   select<
     a extends keyof T,
     b extends keyof T[a],
@@ -45,17 +44,111 @@ export class MockStore<T> extends ReplaySubject<T> {
     key4: d,
     key5: e,
     key6: f
-  ): MockStore<T[a][b][c][d][e][f]>;
+  ): Observable<T[a][b][c][d][e][f]>;
+  /**
+   * This overload is used to support spread operator with
+   * fixed length tuples type in typescript 2.7
+   */
+  select<K = any>(...paths: string[]): Observable<K>;
   select(
     pathOrMapFn: ((state: T) => any) | string,
     ...paths: string[]
-  ): MockStore<any> {
-    let mapped$: MockStore<any>;
+  ): Observable<any> {
+    return select.call(null, pathOrMapFn, ...paths)(this);
+  }
+
+  // Original signature:
+  // dispatch<V extends Action = Action>(action: V): void {
+  /* istanbul ignore next */
+  dispatch() {
+    // Do nothing here. The call is recorded using a Jasmine spy.
+  }
+}
+
+// Function copied from ngrx Store:
+// https://github.com/ngrx/platform/blob/master/modules/store/src/store.ts
+// MIT-licensed like ngrx:
+// https://github.com/ngrx/platform/blob/master/LICENSE
+function select<T, K>(
+  mapFn: (state: T) => K
+): (source$: Observable<T>) => Observable<K>;
+function select<T, a extends keyof T>(
+  key: a
+): (source$: Observable<T>) => Observable<T[a]>;
+function select<T, a extends keyof T, b extends keyof T[a]>(
+  key1: a,
+  key2: b
+): (source$: Observable<T>) => Observable<T[a][b]>;
+function select<
+  T,
+  a extends keyof T,
+  b extends keyof T[a],
+  c extends keyof T[a][b]
+>(
+  key1: a,
+  key2: b,
+  key3: c
+): (source$: Observable<T>) => Observable<T[a][b][c]>;
+function select<
+  T,
+  a extends keyof T,
+  b extends keyof T[a],
+  c extends keyof T[a][b],
+  d extends keyof T[a][b][c]
+>(
+  key1: a,
+  key2: b,
+  key3: c,
+  key4: d
+): (source$: Observable<T>) => Observable<T[a][b][c][d]>;
+function select<
+  T,
+  a extends keyof T,
+  b extends keyof T[a],
+  c extends keyof T[a][b],
+  d extends keyof T[a][b][c],
+  e extends keyof T[a][b][c][d]
+>(
+  key1: a,
+  key2: b,
+  key3: c,
+  key4: d,
+  key5: e
+): (source$: Observable<T>) => Observable<T[a][b][c][d][e]>;
+function select<
+  T,
+  a extends keyof T,
+  b extends keyof T[a],
+  c extends keyof T[a][b],
+  d extends keyof T[a][b][c],
+  e extends keyof T[a][b][c][d],
+  f extends keyof T[a][b][c][d][e]
+>(
+  key1: a,
+  key2: b,
+  key3: c,
+  key4: d,
+  key5: e,
+  key6: f
+): (source$: Observable<T>) => Observable<T[a][b][c][d][e][f]>;
+/**
+ * This overload is used to support spread operator with
+ * fixed length tuples type in typescript 2.7
+ */
+function select<T, K = any>(
+  ...paths: string[]
+): (source$: Observable<T>) => Observable<K>;
+function select<T, K>(
+  pathOrMapFn: ((state: T) => any) | string,
+  ...paths: string[]
+) {
+  return function selectOperator(source$: Observable<T>): Observable<K> {
+    let mapped$: Observable<any>;
 
     if (typeof pathOrMapFn === 'string') {
-      mapped$ = pluck.call(this, pathOrMapFn, ...paths);
+      mapped$ = source$.pipe(pluck(pathOrMapFn, ...paths));
     } else if (typeof pathOrMapFn === 'function') {
-      mapped$ = map.call(this, pathOrMapFn);
+      mapped$ = source$.pipe(map(pathOrMapFn));
     } else {
       throw new TypeError(
         `Unexpected type '${typeof pathOrMapFn}' in select operator,` +
@@ -63,32 +156,28 @@ export class MockStore<T> extends ReplaySubject<T> {
       );
     }
 
-    return distinctUntilChanged.call(mapped$);
-  }
-
-  dispatch<V extends Action = Action>(action: V): void {
-    // Do nothing here. The call is recorded using a Jasmine spy.
-  }
+    return mapped$.pipe(distinctUntilChanged());
+  };
 }
 
 /*
-Returns a mock NgRx Store with the given state.
-For testing components and directives that require the NgRx Store.
+Returns a mock ngrx Store with the given state.
+For testing components and directives that require the ngrx Store.
 
 How to use this:
 
 Create a mock state with just the relevant subtree:
 
-const mockState = {
-  part: <PartState>{ ... }
+const mockState: Partial<AppState> = {
+  part: { ... }
 };
 
 Create a mock store and provide it as a value for the Store dependency:
 
 TestBed.configureTestingModule({
-  providers: {
+  providers: [
     { provide: Store, useValue: makeMockStore(mockState) }
-  },
+  ],
   declarations: [ MyComponentUnderTest ]
 })
 .compileComponents();
@@ -96,7 +185,7 @@ TestBed.configureTestingModule({
 The `dispatch` method is a no-op wrapped by a Jasmine spy.
 To check if the component has dispatched an action:
 
-const store: Store<typeof mockState> = TestBed.get(Store);
+const store: Store<AppState> = TestBed.get(Store);
 
 expect(store.dispatch).toHaveBeenCalledWith(
   new SomeAction()
@@ -105,7 +194,7 @@ expect(store.dispatch).toHaveBeenCalledWith(
   new SomeAction(somePayload)
 );
 
-This mock store only implements the simplest API of an NgRx Store:
+This mock store only implements the simplest API of an ngrx Store:
 `select` and `dispatch`.
 If the component needs a full ngrx store, please use makeStoreModule()
 to create a fully-featured ngrx store.
