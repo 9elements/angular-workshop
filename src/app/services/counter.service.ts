0419addfb3d27231d10c44a2b5edeb5eb314426f
Mathias Schaefer (molily) <molily@mailbox.org>
2018-10-04 17:43:02 +0200

    Provide service in root.

--- src/app/services/counter.service.ts
+++ src/app/services/counter.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
 
-@Injectable()
+@Injectable({
+  providedIn: 'root'
+})
 export class CounterService {
 
   private count = 0;
