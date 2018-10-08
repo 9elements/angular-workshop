2801459cfe1e7950f7c90a5ea3a4f859bedb6871
Mathias Schaefer (molily) <molily@mailbox.org>
2018-10-08 22:21:01 +0200

    Add output.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,4 +5,10 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {}
+export class AppComponent {
+
+  handleChange(count: number) {
+    console.log('change event from IndependentCounter', count);
+  }
+
+}
