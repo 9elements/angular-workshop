119aaad474a94d4f4e86bdaecffc1d5c234c7b12
Mathias Schaefer (molily) <molily@mailbox.org>
2018-10-15 13:45:33 +0200

    Use the click spec helper.

--- src/app/ngrx-counter/ngrx-counter.component.spec.ts
+++ src/app/ngrx-counter/ngrx-counter.component.spec.ts
@@ -3,7 +3,7 @@ import { Store } from '@ngrx/store';
 
 import { Decrement, Increment, Reset } from '../actions/counter.actions';
 import { AppState } from '../shared/app-state';
-import { expectText, findEl } from '../spec-helpers/element.spec-helper';
+import { click, expectText, findEl } from '../spec-helpers/element.spec-helper';
 import { provideMockStore } from '../spec-helpers/mock-store.spec-helper';
 import { NgRxCounterComponent } from './ngrx-counter.component';
 
@@ -34,26 +34,26 @@ describe('NgRxCounterComponent', () => {
   });
 
   it('increments the count', () => {
-    findEl(fixture, 'increment-button').triggerEventHandler('click', null);
+    click(fixture, 'increment-button');
     expect(store.dispatch).toHaveBeenCalledWith(new Increment());
   });
 
   it('decrements the count', () => {
-    findEl(fixture, 'decrement-button').triggerEventHandler('click', null);
+    click(fixture, 'decrement-button');
     expect(store.dispatch).toHaveBeenCalledWith(new Decrement());
   });
 
   it('resets the count', () => {
     const value = 15;
     findEl(fixture, 'reset-input').nativeElement.value = value;
-    findEl(fixture, 'reset-button').triggerEventHandler('click', null);
+    click(fixture, 'reset-button');
     expect(store.dispatch).toHaveBeenCalledWith(new Reset(value));
   });
 
   it('does not reset if the value is not a number', () => {
     const value = 'lol';
     findEl(fixture, 'reset-input').nativeElement.value = value;
-    findEl(fixture, 'reset-button').triggerEventHandler('click', null);
+    click(fixture, 'reset-button');
     expect(store.dispatch).not.toHaveBeenCalled();
   });
 
