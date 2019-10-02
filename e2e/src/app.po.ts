16e4a96552a963f1814833ce7675dbf733ce9b0c
Mathias Schaefer (molily) <molily@mailbox.org>
2019-10-02 11:20:54 +0200

    Clean up and fix e2e specs.

--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -1,11 +1,57 @@
-import { browser, by, element } from 'protractor';
+import {
+  browser,
+  promise,
+  ElementArrayFinder,
+  ElementFinder
+} from 'protractor';
+
+import { findEls } from '../e2e.spec-helper';
 
 export class AppPage {
-  navigateTo() {
-    return browser.get(browser.baseUrl) as Promise<any>;
+  public navigateTo(): promise.Promise<any> {
+    return browser.get('/');
   }
 
-  getTitleText() {
-    return element(by.css('app-root h1')).getText() as Promise<string>;
+  public getCounts(): ElementArrayFinder {
+    return findEls('count');
+  }
+
+  public getCountTexts(): promise.Promise<unknown[]> {
+    return this.getCounts().map(
+      // tslint:disable-next-line: no-non-null-assertion
+      (el) => el!.getText()
+    );
+  }
+
+  public getIncrementButtons(): ElementArrayFinder {
+    return findEls('increment-button');
+  }
+
+  public getIncrementButton(index: number): ElementFinder {
+    return this.getIncrementButtons().get(index);
+  }
+
+  public getDecrementButtons(): ElementArrayFinder {
+    return findEls('decrement-button');
+  }
+
+  public getDecrementButton(index: number): ElementFinder {
+    return this.getDecrementButtons().get(index);
+  }
+
+  public getResetInputs(): ElementArrayFinder {
+    return findEls('reset-input');
+  }
+
+  public getResetInput(index: number): ElementFinder {
+    return this.getResetInputs().get(index);
+  }
+
+  public getResetButtons(): ElementArrayFinder {
+    return findEls('reset-button');
+  }
+
+  public getResetButton(index: number): ElementFinder {
+    return this.getResetButtons().get(index);
   }
 }
