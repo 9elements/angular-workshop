9f4cd97fc48c34a0623a931f5ce1346caa59ca21
Mathias Schaefer (molily) <molily@mailbox.org>
2020-01-07 09:09:37 +0100

    Use data-testid instead of data-qa.

--- e2e/e2e.spec-helper.ts
+++ e2e/e2e.spec-helper.ts
@@ -1,13 +1,19 @@
-import { ElementArrayFinder, ElementFinder, Locator, by, element } from 'protractor';
+import {
+  ElementArrayFinder,
+  ElementFinder,
+  Locator,
+  by,
+  element
+} from 'protractor';
 
-export function queryAttributeLocator(qaAttribute: string): Locator {
-  return by.css(`[data-qa="${qaAttribute}"]`);
+export function queryAttributeLocator(testId: string): Locator {
+  return by.css(`[data-testid="${testId}"]`);
 }
 
-export function findEl(qaAttribute: string): ElementFinder {
-  return element(queryAttributeLocator(qaAttribute));
+export function findEl(testId: string): ElementFinder {
+  return element(queryAttributeLocator(testId));
 }
 
-export function findEls(qaAttribute: string): ElementArrayFinder {
-  return element.all(queryAttributeLocator(qaAttribute));
+export function findEls(testId: string): ElementArrayFinder {
+  return element.all(queryAttributeLocator(testId));
 }
--- src/app/spec-helpers/element.spec-helper.ts
+++ src/app/spec-helpers/element.spec-helper.ts
@@ -6,10 +6,7 @@ import { By } from '@angular/platform-browser';
 
 // Finds a single element with a CSS selector.
 // Throws an error if no element was found.
-function queryByCss<T>(
-  fixture: ComponentFixture<T>,
-  selector: string
-): DebugElement {
+function queryByCss<T>(fixture: ComponentFixture<T>, selector: string): DebugElement {
   // The return type of DebugElement#query() is declared as DebugElement,
   // but the actual return type is DebugElement | null.
   // See https://github.com/angular/angular/issues/22449.
@@ -21,55 +18,44 @@ function queryByCss<T>(
   return debugElement;
 }
 
-// Finds an element with the given 'data-qa' attribute.
+// Finds an element with the given 'data-testid' attribute.
 // Throws an error if no element was found.
-export function findEl<T>(
-  fixture: ComponentFixture<T>,
-  qaAttribute: string
-): DebugElement {
-  return queryByCss<T>(fixture, `[data-qa="${qaAttribute}"]`);
+export function findEl<T>(fixture: ComponentFixture<T>, testId: string): DebugElement {
+  return queryByCss<T>(fixture, `[data-testid="${testId}"]`);
 }
 
-// Finds all elements with the given 'data-qa' attribute.
-export function findEls<T>(
-  fixture: ComponentFixture<T>,
-  qaAttribute: string
-): DebugElement[] {
-  return fixture.debugElement.queryAll(By.css(`[data-qa="${qaAttribute}"]`));
+// Finds all elements with the given 'data-testid' attribute.
+export function findEls<T>(fixture: ComponentFixture<T>, testId: string): DebugElement[] {
+  return fixture.debugElement.queryAll(By.css(`[data-testid="${testId}"]`));
 }
 
-// Gets the text content of an element with the given 'data-qa' attribute.
-export function getText<T>(
-  fixture: ComponentFixture<T>,
-  qaAttribute: string
-): string {
-  return findEl(fixture, qaAttribute).nativeElement.textContent;
+// Gets the text content of an element with the given 'data-testid' attribute.
+export function getText<T>(fixture: ComponentFixture<T>, testId: string): string {
+  return findEl(fixture, testId).nativeElement.textContent;
 }
 
-// Expects that the element with the given 'data-qa' attribute has the given text content.
+// Expects that the element with the given 'data-testid' attribute
+// has the given text content.
 export function expectText<T>(
   fixture: ComponentFixture<T>,
-  qaAttribute: string,
-  text: string
+  testId: string,
+  text: string,
 ): void {
-  expect(getText(fixture, qaAttribute).trim()).toBe(text);
+  expect(getText(fixture, testId).trim()).toBe(text);
 }
 
 // Expects that the component has the given text content.
-export function expectContent<T>(
-  fixture: ComponentFixture<T>,
-  text: string
-): void {
+export function expectContent<T>(fixture: ComponentFixture<T>, text: string): void {
   expect(fixture.nativeElement.textContent.trim()).toBe(text);
 }
 
-// Sets the value of a form field with the given 'data-qa' attribute.
+// Sets the value of a form field with the given 'data-testid' attribute.
 export function setFieldValue<T>(
   fixture: ComponentFixture<T>,
-  qaAttribute: string,
-  value: string
+  testId: string,
+  value: string,
 ): void {
-  findEl(fixture, qaAttribute).nativeElement.value = value;
+  findEl(fixture, testId).nativeElement.value = value;
 }
 
 // Makes a fake click event that provides the most important properties.
@@ -82,16 +68,13 @@ export function makeClickEvent(target: EventTarget): Partial<MouseEvent> {
     target,
     currentTarget: target,
     bubbles: true,
-    cancelable: true
+    cancelable: true,
   };
 }
 
-// Emulates a left click on the element with the given 'data-qa' attribute.
-export function click<T>(
-  fixture: ComponentFixture<T>,
-  qaAttribute: string
-): void {
-  const el = findEl(fixture, qaAttribute);
+// Emulates a left click on the element with the given 'data-testid' attribute.
+export function click<T>(fixture: ComponentFixture<T>, testId: string): void {
+  const el = findEl(fixture, testId);
   const event = makeClickEvent(el.nativeElement);
   el.triggerEventHandler('click', event);
 }
@@ -99,10 +82,10 @@ export function click<T>(
 // Finds a nested component by its selector, e.g. 'app-example'.
 // Throws an error if no element was found.
 // Use this only for shallow component testing.
-// When finding other elements, use findEl(s) and data-qa attributes.
+// When finding other elements, use findEl(s) and data-testid attributes.
 export function findComponent<T>(
   fixture: ComponentFixture<T>,
-  selector: string
+  selector: string,
 ): DebugElement {
   return queryByCss(fixture, selector);
 }
@@ -110,7 +93,7 @@ export function findComponent<T>(
 // Finds all nested components by its selector, e.g. 'app-example'.
 export function findComponents<T>(
   fixture: ComponentFixture<T>,
-  selector: string
+  selector: string,
 ): DebugElement[] {
   return fixture.debugElement.queryAll(By.css(selector));
 }
