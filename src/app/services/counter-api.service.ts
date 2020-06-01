b43e3f2fc630e97a5c70c618a7225eb410dbf363
Mathias Schaefer (molily) <molily@mailbox.org>
2020-06-01 22:20:48 +0200

    Clean up imports

--- src/app/services/counter-api.service.ts
+++ src/app/services/counter-api.service.ts
@@ -1,7 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
-import { catchError } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 @Injectable()
 export class CounterApiService {
