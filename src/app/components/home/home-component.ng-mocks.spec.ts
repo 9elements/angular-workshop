d1dcdb2b1392b4d9c47ee7fc21f420953d981a4e
Mathias Schaefer (molily) <molily@mailbox.org>
2020-11-15 22:21:04 +0100

    Add HomeComponent spec with ng-mocks

---
+++ src/app/components/home/home-component.ng-mocks.spec.ts
@@ -0,0 +1,45 @@
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { MockComponent } from 'ng-mocks';
+
+import { CounterComponent } from '../counter/counter.component';
+import { HomeComponent } from './home.component';
+
+describe('HomeComponent (with ng-mocks)', () => {
+  let fixture: ComponentFixture<HomeComponent>;
+  let component: HomeComponent;
+  let counter: CounterComponent;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [HomeComponent, MockComponent(CounterComponent)],
+      schemas: [NO_ERRORS_SCHEMA],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(HomeComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+
+    const counterEl = fixture.debugElement.query(By.directive(CounterComponent));
+    counter = counterEl.componentInstance;
+  });
+
+  it('renders an independent counter', () => {
+    expect(counter).toBeTruthy();
+  });
+
+  it('passes a start count', () => {
+    expect(counter.startCount).toBe(5);
+  });
+
+  it('listens for count changes', () => {
+    spyOn(console, 'log');
+    const count = 5;
+    counter.countChange.emit(count);
+    expect(console.log).toHaveBeenCalledWith(
+      'countChange event from CounterComponent',
+      count,
+    );
+  });
+});
