2911e8bcd7948c72986833dd5bb0406fe2bd29d9
Mathias Schaefer (molily) <molily@mailbox.org>
2020-11-30 23:47:46 +0100

    Cypress tweaks, consistency with flickr search

--- cypress/plugins/cy-ts-preprocessor.js
+++ cypress/plugins/cy-ts-preprocessor.js
@@ -1,8 +1,8 @@
-const webpackPreprocessor = require("@cypress/webpack-preprocessor");
+const webpackPreprocessor = require('@cypress/webpack-preprocessor');
 
 const webpackOptions = {
   resolve: {
-    extensions: [".ts", ".js"]
+    extensions: ['.ts', '.js'],
   },
   module: {
     rules: [
@@ -11,16 +11,19 @@ const webpackOptions = {
         exclude: [/node_modules/],
         use: [
           {
-            loader: "ts-loader"
-          }
-        ]
-      }
-    ]
-  }
+            loader: 'ts-loader',
+            options: {
+              transpileOnly: true,
+            },
+          },
+        ],
+      },
+    ],
+  },
 };
 
 const options = {
-  webpackOptions
+  webpackOptions,
 };
 
 module.exports = webpackPreprocessor(options);
--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -36,7 +36,9 @@
 //
 //
 // -- This is a dual command --
-// Cypress.Commands.add("dismiss", { prevSubject: 'optional'}, (subject, options) => { ... })
+// Cypress.Commands.add(
+//   "dismiss", { prevSubject: 'optional'}, (subject, options) => { ... }
+// )
 //
 //
 // -- This will overwrite an existing command --
