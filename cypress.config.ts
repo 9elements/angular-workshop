f954f8a5ae47fc53d2e7bd1a2398a8e49133104e
Mathias Schäfer <zapperlott@gmail.com>
2022-07-06 10:44:59 +0200

    Cypress 10 cleanup

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,16 +1,8 @@
-import { defineConfig } from 'cypress'
+import { defineConfig } from 'cypress';
 
 export default defineConfig({
-  videosFolder: 'cypress/videos',
-  screenshotsFolder: 'cypress/screenshots',
-  fixturesFolder: 'cypress/fixtures',
-  video: false,
   e2e: {
-    // We've imported your old cypress plugins here.
-    // You may want to clean this up later by importing these.
-    setupNodeEvents(on, config) {
-      return require('./cypress/plugins/index.ts')(on, config)
-    },
     baseUrl: 'http://localhost:4200',
+    video: false,
   },
-})
+});
