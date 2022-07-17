37c44acd67d69127ffbb0e587d114f4f786718aa
Mathias Schäfer <zapperlott@gmail.com>
2022-07-17 21:23:06 +0200

    Firefox launcher

--- karma.conf.js
+++ karma.conf.js
@@ -8,6 +8,7 @@ module.exports = function (config) {
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
+      require('karma-firefox-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage'),
       require('@angular-devkit/build-angular/plugins/karma')
